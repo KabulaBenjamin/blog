0623b31863d52d00aca12146b9ef44b055346b6b
Benjamin Kabula Koikoi <kabulabenjamin25@gmail.com>
2026-07-10 10:46:27 +0300

    Initial commit: AdventConnect core architecture

--- migrations/20260626_add_editor_type.sql
+++
@@ -1,6 +0,0 @@
--- Add editor_type column to posts table
-ALTER TABLE posts
-ADD COLUMN editor_type VARCHAR(20) DEFAULT 'quill' NOT NULL;
-
--- Update existing rows to default
-UPDATE posts SET editor_type = 'quill' WHERE editor_type IS NULL;
